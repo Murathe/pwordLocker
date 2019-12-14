b43a24163f2cea7167cd42b5a5da88c786ff4459
murathe <isaacsm07p@gmail.com>
2019-12-14 22:54:18 +0300

    Add function to show & delete passwords

--- main.py
+++ main.py
@@ -14,4 +14,20 @@ class Password:
         self.email = email
         self.password = password
 
-    
+    def save_password(self):
+        """
+        Save newly generated passwords to the array
+        """
+        Password.password_list.append(self)
+
+    def show_passwords(self):
+        """
+        Display a list of passwords
+        """
+        return self.Password_list
+
+    def remove_password(self):
+        """
+        This function removes a password from the saved list
+        """
+        Password.Password_list.remove(self)
