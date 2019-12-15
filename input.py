81451786d3e36554eb6e72d4e4f7fa08c0652536
murathe <isaacsm07p@gmail.com>
2019-12-15 19:47:05 +0300

    Addnew/viewall/delete password function

--- input.py
+++ input.py
@@ -68,17 +68,44 @@ def main():
 
     while True:
         command = input(
-            "Here you can 'generate' to generate a new password, 'del' to delete a password, 'add' to add an existing password, 'view' to view all your generated passwords or, 'exist' to leave:( \n"
+            "Here you can 'generate' to generate a new password, 'delete' to delete a password, 'addnew' to add an existing password, 'viewall' to view all your generated passwords or, 'exit' to leave:( \n"
         )
-        if command == "add":
+        if command == "addnew":
             print("Not that you here, let's build a password \n")
             site = input("The site you want a passwor dfor is?\n")
-            user_name = input("A unique username please?")
+            user_name = input("A unique username please?\n")
             password = getpass.getpass("You unique password is?\n")
             
             add_password(password)
-
-
+        elif command == 'generate':
+            print("New passwords are secure\n")
+            site = input("The site you want a passwor dfor is?\n")
+            user_name = input("A unique username please?\n")
+            length = input("How long do you want it to be?\n")
+            
+            try:
+                add_password(password)
+                print(f"Password generated for {site}\n")
+            except ValueError:
+                print("The length value should be a number\n")
+        elif command == 'viewall':
+            if view_passwords():
+                for password in Password.Password_list:
+                    print("-"*6, view_passwords().index(password)+1, "-"*6, "\n")
+                    print(f"Username --> {password.username}\n")
+                    print(f"Password --> {password.password}\n")
+            else:
+                print("Save some passwords and I'll have some to display next time\n")
+        elif command = ='delete':
+            print("This the original rabbit hole, you sure want to delete it?\n")
+            pass_word = getpass.getpass("May I hav the password to be deleted?\n")
+            if pass_word == user_pass:
+                site = input("Give me the site name you no longer needs\n")
+                if password_exists(site):
+                    delete_password(site)
+                    print(f"{site} password has been deleted\n")
+                else:
+                    
 
 
 
