0d3f9e4b044f72ea50a72bc5430b395cab70e8ac
murathe <isaacsm07p@gmail.com>
2019-12-18 16:59:15 +0300

    Re-test

--- userTest.py
+++ userTest.py
@@ -1,28 +1,3 @@
 import unittest
 from user import User
-
-class TestUser(unittest.TestCase):
-    """
-    Contains all tests regarding user module
-    """
-    def setUp(self):
-        """
-        Create new instance before any test
-        """
-        self.new_user = user("murathe", "2233")
-
-    def test_init(self):
-        """
-        Check user validity
-        """
-        self.assertEqual(self.new_user, "murathe")
-        self.assertEqual(self.new_password, "2233")
-
-    def test_user_password(self):
-        """
-        Check password validity
-        """
-        
-    
-if __name__ == '__main__':
-    unittest.main()
+from 
